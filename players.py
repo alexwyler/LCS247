4cdf816c4f54f794f0be59edc36c0512b51a3947
Alex Wyler <alex.s.wyler@gmail.com>
2014-09-13 14:05:37 -0500

    order players
    Summary:
    
    Reviewers: eric, grant, tyrell, jacob, song, kenny, patrickcharlesmayer
    
    Maniphest Tasks:
    
    Reviewed By:
    
    Differential Revision:

--- players.py
+++ players.py
@@ -7,56 +7,40 @@ import collections
 
 PLAYERS = collections.OrderedDict()
 
-PLAYERS['Chaox'] = ['Chaox']
-
-#Dignitas
-PLAYERS['Imaqtpie'] = ['Imaqtpie', 'qtpie', 'looking2sub4lcs', 'Zstinkloser430Z', 'LMQ Tc Tabe', 'Jessica Lynn', 'adsafadfdsafdasf']
-PLAYERS['ZionSpartan'] = ['ZionSpartan'];
-PLAYERS['Crumbz'] = ['Crumbz', 'd Crumbz', 'Crumblina', 'SiAseeTnoAevilN', 'Phd Mundo', 'Sin Pact', 'Prof Crumbledore']
-PLAYERS['Shiptur'] = ['Shiphtur', 'mMe Shiphtur', 'Chapanya', 'CST Shiponya', 'Pawn Dog', 'Apdo Dog']
-PLAYERS['KiWiKiD'] = ['KiWiKiD', 'Snazero8', 'TheShyOne', 'KiwiKid', 'd KiWiKid', 'WORST BOWLHAVIOR']
-
-#EG
-PLAYERS['Pobelter'] = ['Pobelter', 'intero', 'imPobelter', 'Crs Pobelt', 'xPobelter', 'Crs Pobelter', 'Apdo Dog1', 'Wizikx']
-PLAYERS['Innox'] = ['Innox']
-PLAYERS['Helios'] = ['Helios', 'QBR9WQFWE0']
-PLAYERS['Altec'] = ['Altec', 'xiaomx', 'AItecX', 'illustic', 'Crs Altec', 'scarraru', 'AItecRu', 'AltecX', 'C9 Altec']
-PLAYERS['Krepo'] = ['Kreported', 'CLG SkumbagKrepo', 'Busdriver', 'MockingBeard', 'CLG SkumbagKrepo']
-
-#T8
 PLAYERS['PorpoisePops'] = ['PorpoisePops', 'vVv PorpoisePops']
 PLAYERS['Slooshi'] = ['Slooshi', 'aL Slooshi', 'Slooshi8']
 PLAYERS['frommaplestreet'] = ['frommaplestreet', 'Velocity Maple']
 PLAYERS['Dodo8'] = ['Dodo8']
-
-# CLG
-PLAYERS['Seraph'] = ['Seraph', 'anik']
-PLAYERS['Dexter'] = ['dexter', 'GoldPoro2', 'LDdexter', 'OPOP174AMHA195', 'CIJL963PFPA447B', 'CLGsavior', 'CHIMPERATOR']
-PLAYERS['LiNk'] = ['LiNk', 'LiNk115', 'scarra115', 'MidLink', 'linkkk', 'kTLink', 'kTZelda', 'CLGzelda']
+PLAYERS['Imaqtpie'] = ['Imaqtpie', 'qtpie', 'looking2sub4lcs', 'Zstinkloser430Z', 'LMQ Tc Tabe', 'Jessica Lynn', 'adsafadfdsafdasf']
+PLAYERS['Pobelter'] = ['Pobelter', 'intero', 'imPobelter', 'Crs Pobelt', 'xPobelter', 'Crs Pobelter', 'Apdo Dog1', 'Wizikx']
+PLAYERS['ZionSpartan'] = ['ZionSpartan'];
+PLAYERS['Shiptur'] = ['Shiphtur', 'mMe Shiphtur', 'Chapanya', 'CST Shiponya', 'Pawn Dog', 'Apdo Dog']
+PLAYERS['Altec'] = ['Altec', 'xiaomx', 'AItecX', 'illustic', 'Crs Altec', 'scarraru', 'AItecRu', 'AltecX', 'C9 Altec']
 PLAYERS['Aphromoo'] = ['Aphromoo', 'FeaRAphromoo', 'Veinzx', 'nJoyDrop', 'Neruma', 'TheycallmeZorro', 'LegitJJ', 'v8Aphromoo', 'Daphro', 'Zebuum', 'Zeemeeandye', 'CLGAphromoo']
 PLAYERS['DoubleLift'] = ['Doublelift', 'Triplelift', 'Quadralift', 'Pentalift', 'LiftLift', 'Peng Yiliang']
-
-# C9
 PLAYERS['Balls'] = ['BalIs', 'C9 Squirtle', 'C9 Balls']
 PLAYERS['Hai'] = ['hai', 'C9 HyperX Diego', 'C9 Hai', 'AndrossiBaam', 'C9 Hairo']
 PLAYERS['Meteos'] = ['Meteos', 'C10 Meteos', 'C9 Meteos', 'Short Dog', 'Long Dong', 'C9 Axel']
 PLAYERS['Sneaky'] = ['Sneaky', 'C9 StealthBomber', 'C9 Sneaky', 'd SnEaKyCaStRo', 'SnEaKyCaStRoO', 'Pulse CaStRo', 'Heathers Suka', 'AL SnEaKyCaStRo']
 PLAYERS['LemonNation'] = ['LemonNation', 'C9 BananaNation', 'C9 HyperX Dora', 'C9 LemonNation', 'Nom Nom Lemon', 'Quantic Lemon NT', 'oRb LemonNation', 'ShawnOfTheDead', 'LemonBoy']
-
-# Curse
-PLAYERS['Quas'] = ['Quas', 'Crs Quas', 'gg Quas', 'nwe Quas', 'Dr Quas', 'Quasmire']
 PLAYERS['Voyboy'] = ['Voyboy', 'Crs Vooby', 'Crs Voyboy', 'Nihilmatic']
+PLAYERS['Krepo'] = ['Kreported', 'CLG SkumbagKrepo', 'Busdriver', 'MockingBeard', 'CLG SkumbagKrepo']
+PLAYERS['Crumbz'] = ['Crumbz', 'd Crumbz', 'Crumblina', 'SiAseeTnoAevilN', 'Phd Mundo', 'Sin Pact', 'Prof Crumbledore']
+PLAYERS['KiWiKiD'] = ['KiWiKiD', 'Snazero8', 'TheShyOne', 'KiwiKid', 'd KiWiKid', 'WORST BOWLHAVIOR']
+PLAYERS['Innox'] = ['Innox']
+PLAYERS['Helios'] = ['Helios', 'QBR9WQFWE0']
+PLAYERS['Seraph'] = ['Seraph', 'anik']
+PLAYERS['Dexter'] = ['dexter', 'GoldPoro2', 'LDdexter', 'OPOP174AMHA195', 'CIJL963PFPA447B', 'CLGsavior', 'CHIMPERATOR']
+PLAYERS['LiNk'] = ['LiNk', 'LiNk115', 'scarra115', 'MidLink', 'linkkk', 'kTLink', 'kTZelda', 'CLGzelda']
+PLAYERS['Quas'] = ['Quas', 'Crs Quas', 'gg Quas', 'nwe Quas', 'Dr Quas', 'Quasmire']
 PLAYERS['Cop'] = ['Cop', 'Crs Cop', 'v8 Cop', 'EU Cop', 'Entheomancer', 'David Roberson', 'KingWill84', 'crs goompa', 'bootybutts66']
 PLAYERS['XPecial'] = ['Xpecial', 'TSM Xpec']
 
-# Misc
 PLAYERS['Hi Im Gosu'] = ['hi im gosu']
 PLAYERS['TheOddOne'] = ['theoddone']
 PLAYERS['SexyCan'] = ['sexycan']
 PLAYERS['Tossum'] = ['tossum']
 PLAYERS['Much Glorf Wow'] = ['muchglorfwow']
-
-# Random masters
 PLAYERS['Annie Bot'] = ['anniebot']
 PLAYERS['Bwas'] = ['bwas']
 PLAYERS['LattMan'] = ['lattman']
