877aaf7f2c4a7932995bffadb364c77b80170070
Alex Wyler <alex.s.wyler@gmail.com>
2014-09-12 21:58:19 -0500

    working api call
    Summary:
    
    Reviewers: eric, grant, tyrell, jacob, song, kenny, patrickcharlesmayer
    
    Maniphest Tasks:
    
    Reviewed By:
    
    Differential Revision:

--- core/main.py
+++ core/main.py
@@ -3,10 +3,22 @@ Created on Sep 12, 2014
 
 @author: alexwyler
 '''
+from urllib import parse, request
+
+RIOT_CLIENT_KEY = "17e7c567-e54e-4995-bf0f-9d1c9dd3722c"
+RIOT_BASE_URL = "https://na.api.pvp.net/api/lol/na/v1.4"
+
 
 def main():
-    print("Hello World")
+    print(request.urlopen(build_api_url("/summoner/by-name/grraffe")).read())
     pass
+
+def build_api_url(path, params=None):
+    if not params:
+        params = {}
+    
+    params["api_key"] = RIOT_CLIENT_KEY
+    return RIOT_BASE_URL + path + '?' + parse.urlencode(params)
 
 if __name__ == "__main__":
     main()
