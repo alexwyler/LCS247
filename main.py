d8c68cc7ca1bfe15d5c60b2ee327bfafe0386e17
Alex Wyler <alex.s.wyler@gmail.com>
2014-09-12 22:16:52 -0500

    player database
    Summary:
    
    Reviewers: eric, grant, tyrell, jacob, song, kenny, patrickcharlesmayer
    
    Maniphest Tasks:
    
    Reviewed By:
    
    Differential Revision:

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ Created on Sep 12, 2014
 @author: alexwyler
 '''
 from urllib import parse, request
+import players
 
 RIOT_CLIENT_KEY = "17e7c567-e54e-4995-bf0f-9d1c9dd3722c"
 RIOT_BASE_URL = "https://na.api.pvp.net/api/lol/na/v1.4"
 
-
 def main():
-    print(request.urlopen(build_api_url("/summoner/by-name/grraffe")).read())
-    pass
+    
+    for player in players.PLAYERS:
+        for summoner in players.PLAYERS[player]:
+            print(request.urlopen(build_api_url("/summoner/by-name/" + summoner)).read())
+
 
 def build_api_url(path, params=None):
     if not params:
---
+++ players.py
@@ -0,0 +1,9 @@
+'''
+Created on Sep 12, 2014
+
+@author: alexwyler
+'''
+import collections
+
+PLAYERS = collections.OrderedDict()
+PLAYERS['TheOddOne'] = ['TheOddOne'];
