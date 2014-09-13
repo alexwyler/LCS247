be7cc3decbdb0c709842ddb23596d3c189d2f110
Alex Wyler <alex.s.wyler@gmail.com>
2014-09-13 00:30:37 -0500

    working spectation
    Summary:
    
    Reviewers: eric, grant, tyrell, jacob, song, kenny, patrickcharlesmayer
    
    Maniphest Tasks:
    
    Reviewed By:
    
    Differential Revision:

--- main.py
+++ main.py
@@ -6,15 +6,55 @@ Created on Sep 12, 2014
 from urllib import parse, request
 import players
 import json
+import platform
+import subprocess
+import time
 
 RIOT_CLIENT_KEY = "17e7c567-e54e-4995-bf0f-9d1c9dd3722c"
 MASHAPE_BASE_URL = "https://community-league-of-legends.p.mashape.com/api/v1.0/NA/"
 RIOT_BASE_URL = "https://na.api.pvp.net/api/lol/na"
+MAC_LOL_VERSION = "0.0.0.133"
+MAC_LOL_CLIENT_VERSION = "0.0.0.144"
 
 def main():
-    (player, account, game_info) = get_next_game()
-    print(player, account, game_info['playerCredentials'])
+    
+    while True:
+        player, account, game_info = get_next_game()
+        if game_info:
+            spectate_info = game_info['playerCredentials'];   
+            print(player, account, spectate_info)
 
+            if platform.system() == 'Windows':
+                process = open_game_pc(spectate_info)
+            else:
+                process = open_game_mac(spectate_info)
+                
+            while get_active_game(account):
+                print("Game still in progress.  Waiting 30 seconds...")
+                time.sleep(30)
+                
+            process.kill()
+        else:
+            print("No active games.  Waiting 10 seconds...")
+            time.sleep(10)
+        
+'''
+Opens game on mac given the spectate_info and returns a handle on the process
+'''
+def open_game_mac(spectate_info):
+    ip_and_port = str(spectate_info['observerServerIp']) + ':' + str(spectate_info['observerServerPort'])
+    cmd = '''
+    cd /Applications/League\ of\ Legends.app/Contents/LoL/RADS/solutions/lol_game_client_sln/releases/{0}/deploy/LeagueOfLegends.app/Contents/MacOS/
+    riot_launched=true "/Applications/League of Legends.app/Contents/LoL/RADS/solutions/lol_game_client_sln/releases/{0}/deploy/LeagueOfLegends.app/Contents/MacOS/LeagueofLegends" 8394 LoLLauncher "/Applications/League of Legends.app/Contents/LoL/RADS/projects/lol_air_client/releases/{1}/deploy/bin/LolClient" "spectator {2} {3} {4} {5}"
+    '''.format(MAC_LOL_VERSION, MAC_LOL_CLIENT_VERSION, ip_and_port, spectate_info['observerEncryptionKey'], spectate_info['gameId'], 'NA1')
+    
+    print(cmd)
+    full_cmd = ["bash", "-c", cmd]
+    
+    return subprocess.Popen(full_cmd)
+
+def open_game_pc(spectate_info):
+    
 '''
 Returns tuple of (player, account, game_info) for the most popular current game
 '''
@@ -24,7 +64,7 @@ def get_next_game():
             next_game = get_active_game(summoner)
             if next_game:
                 return (player, summoner, next_game)
-    return None
+    return (None, None, None, None)
             
 
 def authenticate_mashape_request(req):
--- players.py
+++ players.py
@@ -6,6 +6,17 @@ Created on Sep 12, 2014
 import collections
 
 PLAYERS = collections.OrderedDict()
-PLAYERS['Hi Im Gosu'] = ['hiimgosu'] 
-PLAYERS['TheOddOne'] = ['theoddone'];
-PLAYERS['SexyCan'] = ['sexycan'];
+
+# CLG
+PLAYERS['Seraph'] = ['Seraph', 'anik']
+PLAYERS['Dexter'] = ['dexter', 'GoldPoro2', 'LDdexter', 'OPOP174AMHA195', 'CIJL963PFPA447B', 'CLGsavior', 'CHIMPERATOR']
+PLAYERS['LiNk'] = ['LiNk', 'LiNk115', 'scarra115', 'MidLink', 'linkkk', 'kTLink', 'kTZelda', 'CLGzelda']
+PLAYERS['Aphromoo'] = ['Aphromoo', 'FeaRAphromoo', 'Veinzx', 'nJoyDrop', 'Neruma', 'TheycallmeZorro', 'LegitJJ', 'v8Aphromoo', 'Daphro', 'Zebuum', 'Zeemeeandye', 'CLGAphromoo']
+
+# C9
+PLAYERS['Hai'] = ['hai', 'C9HyperXDiego', 'C9Hai', 'AndrossiBaam', 'C9Hairo']
+PLAYERS['Hi Im Gosu'] = ['hiimgosu']
+PLAYERS['TheOddOne'] = ['theoddone']
+PLAYERS['SexyCan'] = ['sexycan']
+PLAYERS['Tossum'] = ['tossum']
+PLAYERS['Much Glorf Wow'] = ['muchglorfwow']
