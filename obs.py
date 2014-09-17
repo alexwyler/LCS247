619b47aa6f83fd3bf5ec88ae6500eedcbc900851
Alex Wyler <alex.s.wyler@gmail.com>
2014-09-16 22:32:20 -0500

    fix path
    Summary:
    
    Reviewers: eric, grant, tyrell, jacob, song, kenny, patrickcharlesmayer
    
    Maniphest Tasks:
    
    Reviewed By:
    
    Differential Revision:

--- obs.py
+++ obs.py
@@ -3,20 +3,24 @@ import time
 import os
 import active_games
 
+global OBS_PATH
+
 def init():
+    global OBS_PATH
+    OBS_PATH = os.path.dirname(__file__)
     t = threading.Thread(target=keep_potential_games_up_to_date)
     t.start()
 
 def update_now_playing(player, account, game):
     champ_name = game.get_champion(account[0])
     now_playing = '{0} Playing {1}'.format(player, champ_name)
-    f = open(os.path.join('obs', 'now_playing.txt'), 'w')
+    f = open(os.path.join(OBS_PATH, 'obs', 'now_playing.txt'), 'w')
     f.write(now_playing)
     f.close()
 
 def keep_potential_games_up_to_date():
     while True:
-        f = open(os.path.join('obs', 'potential_games.txt'), 'w')
+        f = open(os.path.join(OBS_PATH, 'obs', 'potential_games.txt'), 'w')
         potential_games = active_games.get_potential_games_in_order()
         if potential_games:
             f.write("--- Players in Game ---\n")
