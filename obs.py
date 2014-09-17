71c842c58a7d1cff8fa68cf9e0281d1c7a18b2e7
Alex Wyler <alex.s.wyler@gmail.com>
2014-09-16 22:42:38 -0500

    carraige return
    Summary:
    
    Reviewers: eric, grant, tyrell, jacob, song, kenny, patrickcharlesmayer
    
    Maniphest Tasks:
    
    Reviewed By:
    
    Differential Revision:

--- obs.py
+++ obs.py
@@ -20,20 +20,20 @@ def update_now_playing(player, account, game):
 
 def keep_potential_games_up_to_date():
     while True:
-        f = open(os.path.join(OBS_PATH, 'obs', 'potential_games.txt'), 'w')
+        f1 = open(os.path.join(OBS_PATH, 'obs', 'potential_games.txt'), 'w')
         potential_games = active_games.get_potential_games_in_order()
         if potential_games:
-            f.write("--- Players in Game ---\n")
+            f1.write("--- Players in Game ---\r\n")
             for suitable_game in potential_games:
                 personality_name, account, score, game = suitable_game
                 if game.is_within_spectator_delay():
-                    waiting = ', waiting to begin\n'
+                    waiting = ', waiting to begin\r\n'
                 else:
                     waiting = ''
-                message = "({0}{3}) {1} on {2} {4}\n".format(score, personality_name, game.get_champion(account[0]), waiting, game.get_timestamp_since_start())
-                f.write(message)
+                message = "({0}{3}) {1} on {2} {4}\r\n".format(score, personality_name, game.get_champion(account[0]), waiting, game.get_timestamp_since_start())
+                f1.write(message)
         else:
-            f.write("No potential games!\n")
+            f1.write("No potential games!\r\n")
         
-        f.close()
+        f1.close()
         time.sleep(5)
